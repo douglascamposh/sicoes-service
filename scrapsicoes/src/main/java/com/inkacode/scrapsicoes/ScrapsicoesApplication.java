346c4c751c1f9ad1a7d3ac68433055f1898836af
douglas-campos-coderoad-com <50142435+douglas-campos-coderoad-com@users.noreply.github.com>
2024-04-03 19:02:19 -0400

    added the project initial configuration

---
+++ scrapsicoes/src/main/java/com/inkacode/scrapsicoes/ScrapsicoesApplication.java
@@ -0,0 +1,13 @@
+package com.inkacode.scrapsicoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapsicoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapsicoesApplication.class, args);
+	}
+
+}
---
+++ scrapsicoes/src/test/java/com/inkacode/scrapsicoes/ScrapsicoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inkacode.scrapsicoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapsicoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
